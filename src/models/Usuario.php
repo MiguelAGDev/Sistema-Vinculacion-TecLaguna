39674a1a8ff6750d3eee31b00ffc934622db24c7
azuu-rgb <azurodriguezflores27@gmail.com>
2025-11-15 13:55:03 -0600

    16 DE NOV DEL 2025 SE AGREGADORN LOS PERMISOS Y DOS METODOS

--- src/models/Usuario.php
+++ src/models/Usuario.php
@@ -15,30 +15,35 @@ require_once __DIR__. '/../../database/Conexion.php';
                   WHERE correo_usuario = :correo";
 
             $stmt = oci_parse($this->conn,$sql);
-            oci_bind_by_name ($stmt,':correo',$correo);
+                    oci_bind_by_name ($stmt,':correo',$correo);
 
+             $sql2 ="INSERT INTO sesion (id_usuario,fecha_inicio,fecha_fin,exito_inicio)
+                     VALUES (:id_usuario,SYSDATE,NULL,:exito_sesion)";
+            $exito_sesion = 0;                              
             if(oci_execute($stmt)){
                 $row = oci_fetch_assoc ($stmt); //para recuperar el reglon y poder acceder a a la comlumna;
                 if($row){ //si todo sale bien se pasa a verificar;
                     $hashGuardado = $row['CONTRASENA_USUARIO']; //podemos accceder a la contraseña hassheada
                     if(password_verify($contrasena,$hashGuardado)){
-                        header("Location: main.php");
+                        $exito_sesion=1;
+                        $stmt2= oci_parse($this->conn,$sql2);
+                                oci_bind_by_name ($stmt2,':id_usuario',$row['ID_USUARIO']);
+                                oci_bind_by_name ($stmt2,':exito_sesion',$exito_sesion);
+                                oci_execute($stmt2);
+                               header("Location: main.php");
                     }else{
                         echo"<p style='color:red'> Contraseña incorrecto </p>";
-                        return false;
                     }   
                 }
-                echo "<p style='color:red'> Usuario no encontrado </p>";
-                return false;
+            }else{
+                 echo "<p style='color:red'> Usuario no encontrado </p>";
             }
-            echo "<p style='color:red'> Error en la cosulta </p>";
-            return false;
+            
+            $stmt2= oci_parse($this->conn,$sql2);
+                    oci_bind_by_name ($stmt2,':id_usuario',$row['ID_USUARIO']);
+                    oci_bind_by_name ($stmt2,':exito_sesion',$exito_sesion);
+                    oci_execute($stmt2);
         }
-        public function registroSesion($id_correo){
-            $sql =$sql="SELECT *
-                  FROM usuario 
-                  WHERE correo_usuario = :correo";"
-
-        }
+       
 
     }
